2a63da3c5188452c2090c814ee3055c1ac56032c
s1f10240260 <s1f102402608@iniad.org>
2026-07-01 22:48:56 +0900

    詳細ページのテストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -89,3 +89,18 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
         self.assertEqual(response.context['tasks'][2], task3)
+
+    def test_detail_get_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.get(f'/todo/{task.id}/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/detail.html')
+        self.assertEqual(response.context['task'], task)
+
+    def test_detail_get_fail(self):
+        client = Client()
+        response = client.get('/todo/1/')
+        self.assertEqual(response.status_code, 404)
